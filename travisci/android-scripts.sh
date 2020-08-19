58446315ae4114c38fd6a992dabe0f9d2c5e4461
BRIAN CHEN <wl00387719@gmail.com>
2020-08-19 17:15:55 +0800

    test

--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,4 +1,9 @@
 export PATH=$HOME/.nvm/versions/node/v12.18.3/bin:$PATH
 detox build -c android.emu.release
-# $ANDROID_HOME/emulator/emulator -verbose -no-window -no-audio -no-boot-anim -wipe-data -gpu swiftshader_indirect @Pixel_3_API_29 &
+$ANDROID_HOME/emulator/emulator -verbose -no-window -no-audio -no-boot-anim -wipe-data -gpu swiftshader_indirect @Pixel_3_API_29 &
+ android-wait-for-emulator
+ adb shell settings put global window_animation_scale 0
+ adb shell settings put global transition_animation_scale 0
+ adb shell settings put global animator_duration_scale 0
+ adb shell input keyevent 82
 detox test -c android.emu.release --cleanup
