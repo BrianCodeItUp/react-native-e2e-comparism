e9604eff48b82481e663f25e92b5a4ba25f42481
BRIAN CHEN <wl00387719@gmail.com>
2020-08-19 15:12:18 +0800

    fix android before install  script

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -3,4 +3,4 @@ echo yes | sdkmanager tools
 # Install the system image.
 echo yes | sdkmanager "system-images;android-29;default;x86_64"
 # Create and start emulator for the script. Meant to race the install task.
-echo no | avdmanager create avd -n  -d Pixel_3_API_29 --package "system-images;android-29;default;x86_64"
+echo no | avdmanager create avd -n Pixel_3_API_29 -d pixel --package "system-images;android-29;default;x86_64"
