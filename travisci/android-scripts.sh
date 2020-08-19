003eda0d9c0a3d090b60b15fb27207867206446f
BRIAN CHEN <wl00387719@gmail.com>
2020-08-19 09:38:45 +0800

    fix open device name in android script

--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,6 +1,6 @@
 export PATH=$HOME/.nvm/versions/node/v12.18.3/bin:$PATH
 detox build -c android.emu.release
-$ANDROID_HOME/emulator/emulator -avd detoxTestEmulator -no-window -noaudio -no-boot-anim -wipe-data &
+$ANDROID_HOME/emulator/emulator -avd Pixel_3_API_29 -no-window -noaudio -no-boot-anim -wipe-data &
  android-wait-for-emulator
  adb shell settings put global window_animation_scale 0
  adb shell settings put global transition_animation_scale 0
