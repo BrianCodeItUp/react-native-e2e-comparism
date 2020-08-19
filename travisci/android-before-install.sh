8241440efd4bf08423f37fa8b2171f1779304608
BRIAN CHEN <wl00387719@gmail.com>
2020-08-19 14:31:48 +0800

    test with android 24 image

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -1,6 +1,6 @@
 # Install the rest of tools (e.g. avdmanager)
 echo yes | sdkmanager tools
 # Install the system image.
-echo yes | sdkmanager "system-images;android-28;default;x86_64"
+echo yes | sdkmanager "system-images;android-24;default;armeabi-v7a"
 # Create and start emulator for the script. Meant to race the install task.
-echo no | avdmanager create avd -n Pixel_API_28_AOSP -d pixel --package "system-images;android-28;default;x86_64"
+echo no | avdmanager create avd -n Pixel_API_24 -d pixel --package "system-images;android-24;default;armeabi-v7a"
--- travisci/android-scripts.sh
+++ travisci/android-scripts.sh
@@ -1,6 +1,6 @@
 export PATH=$HOME/.nvm/versions/node/v12.18.3/bin:$PATH
 detox build -c android.emu.release
-$ANDROID_HOME/emulator/emulator -verbose -no-window -no-audio -no-boot-anim -wipe-data -gpu swiftshader_indirect @Pixel_API_28_AOSP &
+$ANDROID_HOME/emulator/emulator -verbose -no-window -no-audio -no-boot-anim -wipe-data -gpu swiftshader_indirect @Pixel_API_24 &
  android-wait-for-emulator
  adb shell settings put global window_animation_scale 0
  adb shell settings put global transition_animation_scale 0
