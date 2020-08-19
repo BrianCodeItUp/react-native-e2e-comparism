d30d478b2542d11122c40b8c848ba9f2d83b5a08
BRIAN CHEN <wl00387719@gmail.com>
2020-08-19 10:46:03 +0800

    change emulator image to system-images;android-28;default;x86_64

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -1,6 +1,6 @@
 # Install the rest of tools (e.g. avdmanager)
 echo yes | sdkmanager tools
 # Install the system image.
-echo yes | sdkmanager "system-images;android-29;default;x86"
+echo yes | sdkmanager "system-images;android-28;default;x86_64"
 # Create and start emulator for the script. Meant to race the install task.
-echo no | avdmanager create avd --force -n Pixel_3_API_29 -k "system-images;android-29;default;x86"
+echo no | avdmanager create avd -n Pixel_API_28_AOSP -d pixel --package "system-images;android-28;default;x86_64"
