6d44cb726efd121a6ad8694ea9a4021663c37e66
BRIAN CHEN <wl00387719@gmail.com>
2020-08-19 14:59:07 +0800

    test script with android 29

--- travisci/android-before-install.sh
+++ travisci/android-before-install.sh
@@ -1,6 +1,6 @@
 # Install the rest of tools (e.g. avdmanager)
 echo yes | sdkmanager tools
 # Install the system image.
-echo yes | sdkmanager "system-images;android-24;default;armeabi-v7a"
+echo yes | sdkmanager "system-images;android-29;default;x86_64"
 # Create and start emulator for the script. Meant to race the install task.
-echo no | avdmanager create avd -n Pixel_API_24 -d pixel --package "system-images;android-24;default;armeabi-v7a"
+echo no | avdmanager create avd -n  -d Pixel_3_API_29 --package "system-images;android-29;default;x86_64"
