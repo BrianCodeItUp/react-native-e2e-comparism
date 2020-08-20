7250be2e648c8865391c9d2c0bb299f1609032b4
BRIAN CHEN <wl00387719@gmail.com>
2020-08-20 15:42:16 +0800

    set travis wait for ios build

--- travisci/ios-scripts.sh
+++ travisci/ios-scripts.sh
@@ -1,4 +1,4 @@
   
 export PATH=$HOME/.nvm/versions/node/v12.18.3/bin:$PATH
-set -o pipefail && detox build --configuration ios.sim.release
+set -o pipefail && travis_wait 60 detox build --configuration ios.sim.release
 detox test --configuration ios.sim.release --cleanup
